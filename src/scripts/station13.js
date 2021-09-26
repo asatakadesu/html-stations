ceda708cc5db8ad5b59b8cdf2a2986d2b46035ff
asatakadesu <89711024+asatakadesu@users.noreply.github.com>
2021-09-26 17:27:56 +0900

    station13 first

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,6 @@
-function getData() {
-    // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
-    return // ここにアロー関数の処理を書く
+// const { isArrowFunction } = require("typescript");
+
+getData = ()=> {
+    return 'Arrow'// Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
+    return ArrowFunction;// ここにアロー関数の処理を書く
 }
