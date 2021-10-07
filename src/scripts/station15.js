a29d6d347b9319eb68fb635727e97e624ccb0677
asatakadesu <89711024+asatakadesu@users.noreply.github.com>
2021-10-07 22:11:44 +0900

    2021/10/07/22:11

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList) 
   return await result
 }
 
-function test() {
-  return
-}
+function test(data2) {
 
+  return new Promise(function(resolve) {
+    setTimeout(function(){
+      data2.map( (data)=> { 
+        const buildFullName = data.family_name + ' ' + data.first_name
+        data['full_name'] = buildFullName
+        // return data
+      });
+      resolve(data2);
+    }, 3000);
+  });
+}
